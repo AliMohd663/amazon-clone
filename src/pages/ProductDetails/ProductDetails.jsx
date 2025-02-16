a2c4e617404d7cc26ed712f9566dbb80511a59f5
Ali <aliw663.am@gmail.com>
2025-02-16 07:57:12 +0300

    Detail Page Routing

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import styles from './productdetails.module.css'
+import React, { useEffect, useState } from 'react'
 import LayOut from '../../Components/LayOut/LayOut'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { productURL } from '../../API/endpoint'
+import ProductCard from '../../Components/Product/ProductCard'
+
+
 function ProductDetails() {
+
+  const { productId } = useParams()
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    axios.get(`${productURL}/products/${productId}`)
+      .then((res) => {
+        setProduct(res.data)
+      }).catch((err) => {
+        console.log(err)
+      })
+
+
+  }, [])
+
+
+
+
   return (
     <LayOut>
-      <div>ProductDetails</div>
+      <ProductCard
+      
+      product={product}
+      
+      />
     </LayOut>
   )
 }
--- src/pages/Routing.jsx
+++ src/pages/Routing.jsx
@@ -6,7 +6,7 @@ import Payments from './Payment/Payment'
 import Orders from './Orders/Orders'
 import Cart from './Cart/Cart'
 import Results from './Results/Results';
-
+import ProductDetails from './ProductDetails/ProductDetails'
 function Routing() {
   return (
     <Router>
@@ -16,8 +16,11 @@ function Routing() {
         <Route path='/auth' element={<Signup />} />
         <Route path='/payments' element={<Payments />} />
         <Route path='/orders' element={<Orders />} />
-        <Route path='/cart' element={<Cart />} />
         <Route path='/category/:categoryName' element={<Results/>}/>
+       
+        <Route path='/products/:productId' element={ <ProductDetails/>} />
+        <Route path='/cart' element={<Cart />} />
+        
       </Routes>
     </Router>
   )
